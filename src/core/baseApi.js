e1193c5fc16c19a2f7dd4e656fad1c36098fd398
Pyaesone Khant <pyaepsk@gmail.com>
2024-03-18 13:19:32 +0630

    change api

--- src/core/baseApi.js
+++ src/core/baseApi.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
     reducerPath: "baseApi",
     baseQuery: fetchBaseQuery({
-        // baseUrl: "https://blogapp-apis.onrender.com/api",
-        baseUrl: "http://localhost:3500/api",
+        baseUrl: "https://blogapp-apis.onrender.com/api",
+        // baseUrl: "http://localhost:3500/api",
         prepareHeaders: ((headers) => {
             const token = JSON.parse(localStorage.getItem("accessToken"));
             if (token) {
